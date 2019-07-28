506dc1078f6c3998023141ac6268cae62d5f4ab9
gspalato <3950465+gspalato@users.noreply.github.com>
2019-07-28 00:37:36 -0300

    Actually fixed URL recognition... I think.

--- src/client.ts
+++ src/client.ts
@@ -44,11 +44,11 @@ export default class BeamClient {
     public async resolve(search: string, issuer: any): Promise<IData> {
         const node = this.nodes[0];
 
-        const regex: RegExp[] = [
-            /^(https?:\/\/)?(www\.|player\.)?vimeo\.com\/(?:channels\/(?:\w+\/)?|groups\/([^\/]*)\/videos\/|video\/|)(\d+)(?:|\/\?)$/,
-            /^(https?:\/\/)?(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|playlist\?list=|\&v=)(&list=)?([^#\&\?]*).*$/,
+        const regex = [
+            /^(http|https)?:\/\/(www\.|player\.)?vimeo\.com\/(?:channels\/(?:\w+\/)?|groups\/([^\/]*)\/videos\/|video\/|)(\d+)(?:|\/\?)$/,
+            /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|playlist\?list=|\&v=)(&list=)?([^#\&\?]*).*$/,
             /^(https?:\/\/)?(www.)?(m\.)?soundcloud\.com\/[\w\-\.]+(\/)+[\w\-\.]+$/,
-            /^(https?:\/\/)?(clips\.)?twitch\.tv\/(\S+)$/
+            /^(?:https:\/\/)?(clips\.)?twitch\.tv\/(\S+)$/
         ];
 
         let param;
